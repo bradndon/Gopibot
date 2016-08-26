b3c13a552a9791372443826f4acca9c953b31878
Brandon Fox <t-brafox@expedia.com>
2016-08-25 17:29:35 -0700

    ASDF

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from __future__ import print_function
 import httplib2
 import os
 import time
