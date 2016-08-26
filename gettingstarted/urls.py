7f376872cb9b9d0f65f07d13dfd701729584fbfc
Brandon Fox <t-brafox@expedia.com>
2016-08-26 09:52:35 -0700

    Updated url paths

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,7 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^trucks$', hello.views.index, name='index'),
+    url(r'^recommend$', hello.views.recommend, name='recommend'),
     url(r'^admin/', include(admin.site.urls)),
 ]
